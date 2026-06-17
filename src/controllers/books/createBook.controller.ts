5c493746660698063d6d8617276b85ce424c057e
HarshDev1809 <harsh.dev182000@gmail.com>
2026-06-17 14:38:04 +0000

    added books creation

--- src/controllers/books/createBook.controller.ts
+++ src/controllers/books/createBook.controller.ts
@@ -1,13 +1,25 @@
+import db from "../../db/index.js";
+import { book } from "../../db/schema.js";
 import { catchAsync } from "../../utils/catchAsync.js";
 import { NextFunction,Response, Request } from "express";
 
 export const createBook = catchAsync(async (req : Request,res : Response,next : NextFunction)=>{
+
+    const {body} = req;
+    const {name, baseAmount} = body;
+    const {id : userId} = req.user;
+
+    await db.insert(book).values({
+        name,
+        userId,
+        baseAmount,
+        });
+
     return res.status(200).json({
         message : "Book created successfully",
         success : true,
         data : {
-            session : req.session,
-            user : req.user
+            
         }
     })
 })
--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -3,9 +3,11 @@ import { isAuthenticated } from "../middlewares/auth/isAuthenticated.middleware.
 import { validate } from "../middlewares/common/validate.middleware.js";
 import { createBookSchema } from "../schemas/books/createBook.schema.js";
 import { createBook } from "../controllers/books/createBook.controller.js";
+import { getBooks } from "../controllers/books/getBooks.controller.js";
 
 const router = express.Router();
 
-router.post("/books", [isAuthenticated, validate(createBookSchema)],createBook);
+router.post("/", [isAuthenticated, validate(createBookSchema)],createBook);
+router.get("/", [isAuthenticated],getBooks);
 
 export default router;
