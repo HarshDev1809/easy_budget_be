71c1eda6cc382bebb36367bc9e317d7bed079d5f
Harsh Dev <138294344+HarshDev1809@users.noreply.github.com>
2026-06-23 23:33:56 +0530

    Update CORS origin for production environment

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://easy-budget.devbydev.in',
     credentials: true
 }));
 // app.all("/api/auth/*all", async (req, res) => {
