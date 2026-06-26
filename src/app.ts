ae20bacf958653c30bed6996ba84591b46988779
Harsh Dev <138294344+HarshDev1809@users.noreply.github.com>
2026-06-27 00:54:44 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-    // origin: 'https://easy-budget.devbydev.in',
-    origin: 'http://localhost:3000',
+    origin: 'https://easy-budget.devbydev.in',
+   // origin: 'http://localhost:3000',
     credentials: true
 }));
 // app.all("/api/auth/*all", async (req, res) => {
